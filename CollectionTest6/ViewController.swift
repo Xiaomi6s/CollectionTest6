0c6a86fb1f8e3ca2cb1625ad504fdff60cb1654d
renxiaojian <renxiaojian@yonglibao.com>
2017-07-10 10:23:02 +0800

    Initial Commit

---
+++ CollectionTest6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionTest6
+//
+//  Created by rxj on 2017/7/10.
+//  Copyright © 2017年 renxiaojian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
